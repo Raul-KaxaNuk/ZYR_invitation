90088585eb037dbd063ae3f8a57604442018ba10
Raul Silva <raul.silva@kaxanuk.mx>
2023-11-27 20:25:30 -0600

    first commit
    a ver que sale caray

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.image('inputs/Home.png')
+
+st.title('RAUL Y ZANYA')
+st.markdown('lol xd mafia')
+
+# [theme]
+# base="light"
+# primaryColor="#879287"
+# backgroundColor="#8efbac"
+# secondaryBackgroundColor="#a6abb5"
